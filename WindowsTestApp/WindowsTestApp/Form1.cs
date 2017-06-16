7ccc69fb812a87c14b1f1c6ce6ace2b4db5e83a4
tixey <tixey@mail.ru>
2017-06-16 18:33:08 +0400

    Version 4

--- WindowsTestApp/WindowsTestApp/Form1.cs
+++ WindowsTestApp/WindowsTestApp/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsTestApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Hello World!" + " version 3";
+            textBox1.Text = "Hello World!" + " version 3"+"  version4";
         }
 
         private void Form1_Load(object sender, EventArgs e)
